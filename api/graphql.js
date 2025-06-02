ad775b614b2c1458243e3dfcc0541c53c3769d44
João Victor <joao.victor@internit.net.br>
2025-06-02 15:44:16 -0300

    adjust on cors

--- api/graphql.js
+++ api/graphql.js
@@ -1,3 +1,4 @@
+import Cors from 'cors';
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { config } from 'dotenv';
@@ -7,20 +8,28 @@ import { connectToDb } from '../src/database/db.js';
 import { immobileTypes } from '../src/graphql/schema.js';
 import { resolvers } from '../src/graphql/resolvers.js';
 
+const cors = Cors({
+  origin: ['http://localhost:4200'],
+  credentials: true,
+});
+
 const server = new ApolloServer({
   typeDefs: immobileTypes,
   resolvers,
-  introspection: true // permite schema explorer em produção
+  introspection: true,
 });
 
-const handler = startServerAndCreateNextHandler(server, {
-  context: async () => {
-    await connectToDb();
-    return {};
-  },
-  cors: {
-    origin: 'http://localhost:4200',
-  },
-});
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) =>
+      result instanceof Error ? reject(result) : resolve(result)
+    );
+  });
+}
 
-export default handler;
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
+  await connectToDb();
+  const apolloHandler = startServerAndCreateNextHandler(server);
+  return apolloHandler(req, res);
+}
